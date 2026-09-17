3d52af903e3974450a78e287148d169a8102fe5b
Repaflem <benoit.aillet@gmail.com>
2026-09-17 18:35:43 +0200

    Update jeu.js

--- js/jeu.js
+++ js/jeu.js
@@ -1,1 +1,21 @@
-alert("JavaScript fonctionne !");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const bouton = document.querySelector(".game-start-button");
+
+    console.log("jeu.js est chargé");
+    console.log("Bouton trouvé :", bouton);
+
+    if (!bouton) {
+        alert("ERREUR : le bouton n'a pas été trouvé.");
+        return;
+    }
+
+    bouton.addEventListener("click", function (event) {
+
+        event.preventDefault();
+
+        alert("LE BOUTON FONCTIONNE !");
+
+    });
+
+});
